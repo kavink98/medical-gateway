273a21f459372f4cafca7a7bfdaa63e9fd6066e4
Kavin <kavink98@gmail.com>
2023-06-23 16:50:15 -0400

    Update

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const { Gateway, Wallets } = require('fabric-network');
+const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
 // Create an Express app
 const app = express();
-app.use(express.json());
+app.use(cors(), express.json());
 
 // Configure the connection profile and network
 const connectionProfilePath = path.resolve(__dirname, '..', 'fabric-samples', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
 
 // Configure the file system wallet
-const resolveWallet = (org) => {
-    const walletPath = path.resolve(__dirname, 'org1-wallet');
+const walletPath = (org) => {
+    return path.resolve(__dirname, `${org}-wallet`);
 }
 
 // Configuration for the contract and channel
@@ -28,7 +29,8 @@ function handleError(res, error) {
 // Endpoint to invoke the initLedger function
 app.get('/api/initLedger', async (req, res) => {
     try {
-        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        const org = req.header('org')
+        const wallet = await Wallets.newFileSystemWallet(walletPath(org));
         const gateway = new Gateway();
         await gateway.connect(JSON.parse(fs.readFileSync(connectionProfilePath, 'utf-8')), {
             wallet: wallet,
@@ -41,21 +43,75 @@ app.get('/api/initLedger', async (req, res) => {
         const contract = network.getContract(contractName);
 
         // Invoke the initLedger function
-        await contract.submitTransaction('initLedger');
+        const response = await contract.submitTransaction('initLedger');
+        res.status(200).json({ message: 'Ledger initialized successfully.', response: response.toString() });
 
-        // Disconnect from the gateway
-        await gateway.disconnect();
-
-        res.status(200).json({ message: 'Ledger initialized successfully.' });
     } catch (error) {
         handleError(res, error);
     }
 });
 
-// Endpoint to invoke the getMedicalData function
+app.get('/api/medicaldata', async (req, res) => {
+    try {
+      const org = req.header('org');
+      const wallet = await Wallets.newFileSystemWallet(walletPath(org));
+      const gateway = new Gateway();
+      await gateway.connect(
+        JSON.parse(fs.readFileSync(connectionProfilePath, 'utf-8')),
+        {
+          wallet: wallet,
+          identity: 'admin',
+          discovery: { enabled: true, asLocalhost: true },
+        }
+      );
+  
+      // Access the network
+      const network = await gateway.getNetwork(channelName);
+      const contract = network.getContract(contractName);
+  
+      // Invoke the getMedicalRecordsByProvider function
+      const providerID = req.params.providerID;
+      const result = await contract.evaluateTransaction(
+        'getAllMedicalData',
+      );
+  
+      // Disconnect from the gateway
+      await gateway.disconnect();
+  
+      res.status(200).json(JSON.parse(result.toString()));
+    } catch (error) {
+      handleError(res, error);
+    }
+  });
+
+app.delete('/api/medicaldata/:id', async (req, res) => {
+    try {
+        const org = req.header('org')
+        const wallet = await Wallets.newFileSystemWallet(walletPath(org));
+        const gateway = new Gateway();
+        await gateway.connect(JSON.parse(fs.readFileSync(connectionProfilePath, 'utf-8')), {
+            wallet: wallet,
+            identity: 'admin',
+            discovery: { enabled: true, asLocalhost: true },
+        });
+        const network = await gateway.getNetwork(channelName);
+        const contract = network.getContract(contractName);
+
+        const id = req.params.id;
+        const result = await contract.submitTransaction('deletePublicData', id);
+
+        // Disconnect from the gateway
+
+        res.status(200).json({message: "Date removed from public ledger"});
+    } catch (error) {
+        handleError(res, error);
+    }
+});
+
 app.get('/api/medicaldata/:id', async (req, res) => {
     try {
-        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        const org = req.header('org')
+        const wallet = await Wallets.newFileSystemWallet(walletPath(org));
         const gateway = new Gateway();
         await gateway.connect(JSON.parse(fs.readFileSync(connectionProfilePath, 'utf-8')), {
             wallet: wallet,
@@ -81,7 +137,8 @@ app.get('/api/medicaldata/:id', async (req, res) => {
 // Endpoint to invoke the getMedicalDataByPatientID function
 app.get('/api/medicaldata/patient/:patientID', async (req, res) => {
     try {
-        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        const org = req.header('org')
+        const wallet = await Wallets.newFileSystemWallet(walletPath(org));
         const gateway = new Gateway();
         await gateway.connect(JSON.parse(fs.readFileSync(connectionProfilePath, 'utf-8')), {
             wallet: wallet,
@@ -109,7 +166,8 @@ app.get('/api/medicaldata/patient/:patientID', async (req, res) => {
 // Endpoint to invoke the createMedicalData function
 app.post('/api/medicaldata', async (req, res) => {
     try {
-        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        const org = req.header('org')
+        const wallet = await Wallets.newFileSystemWallet(walletPath(org));
         const gateway = new Gateway();
         await gateway.connect(JSON.parse(fs.readFileSync(connectionProfilePath, 'utf-8')), {
             wallet: wallet,
@@ -118,14 +176,12 @@ app.post('/api/medicaldata', async (req, res) => {
         });
 
         // Access the network
-        console.log("Here");
         const network = await gateway.getNetwork(channelName);
         const contract = network.getContract(contractName);
 
         // Invoke the createMedicalData function
         const { id, patientID, patientName, diagnosis, medications } = req.body;
         await contract.submitTransaction('createMedicalData', id, patientID, patientName, diagnosis, medications);
-        await gateway.disconnect();
 
         res.status(200).json({ message: 'Medical data created successfully.' });
     } catch (error) {
@@ -133,33 +189,33 @@ app.post('/api/medicaldata', async (req, res) => {
     }
 });
 
-// Endpoint to invoke the transferMedicalData function
-app.put('/api/medicaldata/:id', async (req, res) => {
+app.get('/api/medicaldata/:id/transfer', async (req, res) => {
     try {
-        const wallet = await Wallets.newFileSystemWallet(walletPath);
-        const gateway = new Gateway();
-        await gateway.connect(JSON.parse(fs.readFileSync(connectionProfilePath, 'utf-8')), {
-            wallet: wallet,
-            identity: 'admin',
-            discovery: { enabled: true, asLocalhost: true },
-        });
-        // Access the network
-        const network = await gateway.getNetwork(channelName);
-        const contract = network.getContract(contractName);
-
-        // Invoke the transferMedicalData function
-        const id = req.params.id;
-        const newOwner = req.body.newOwner;
-        await contract.submitTransaction('transferMedicalData', id, newOwner);
-
-        // Disconnect from the gateway
-        await gateway.disconnect();
-
-        res.status(200).json({ message: 'Medical data transferred successfully.' });
+      const org = req.header('org');
+      const wallet = await Wallets.newFileSystemWallet(walletPath(org));
+      const gateway = new Gateway();
+      await gateway.connect(
+        JSON.parse(fs.readFileSync(connectionProfilePath, 'utf-8')),
+        {
+          wallet: wallet,
+          identity: 'admin',
+          discovery: { enabled: true, asLocalhost: true },
+        }
+      );
+  
+      // Access the network
+      const network = await gateway.getNetwork(channelName);
+      const contract = network.getContract(contractName);
+  
+      // Invoke the transferMedicalData function
+      const id = req.params.id;
+      const response = await contract.submitTransaction('transferMedicalData', id);
+  
+      res.status(200).json({ message: 'Medical data transferred successfully.', response: response.toString() });
     } catch (error) {
-        handleError(res, error);
+      handleError(res, error);
     }
-});
+  });
 
 // Start the Express server
 const port = 3000;
